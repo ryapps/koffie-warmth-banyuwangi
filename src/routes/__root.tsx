eac77f8ebb591b90772a8102d547bea49e891b3c
justwaitryan <you@example.com>
2026-08-09 22:51:14 +0700

    fix:admin

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { type ReactNode } from "react";
 
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "../components/ui/sonner";
 import appCss from "../styles.css?url";
 
 function NotFoundComponent() {
